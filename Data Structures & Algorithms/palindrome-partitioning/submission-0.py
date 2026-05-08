fb20d815908e7e4b0a3bb278deb6fe53b1e4a943
Arnab <89312965+Peanutbrain-pro@users.noreply.github.com>
2026-05-09 01:06:07 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+
+        # take only the left partition
+
+        def dfs(left, right, comb):
+            print(left, right, s[left: right+1], comb)
+            if not self.isPalindrome(s[left: right+1]):
+                return
+
+            comb.append(s[left: right+1])
+            if right == len(s) - 1:
+                # this selection is at the end
+                result.append(comb.copy())
+                comb.pop()
+                return
+
+            # Not at the end
+            for newRight in range(right+1, len(s)):
+                dfs(right+1, newRight, comb)
+                
+            comb.pop()
+
+        temp = []
+        for i in range(len(s)):
+            dfs(0, i, temp)
+        return result
+    
+    def isPalindrome(self, s):
+        return s == s[::-1]
